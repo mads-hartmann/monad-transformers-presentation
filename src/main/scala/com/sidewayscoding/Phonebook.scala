f568cfc05d5c78648a49a19f6fc21e94d1bf4746
Mads Hartmann <mads379@gmail.com>
2012-03-12 11:10:04 +0100

    cleanup and added more state

--- src/main/scala/com/sidewayscoding/Phonebook.scala
+++ src/main/scala/com/sidewayscoding/Phonebook.scala
@@ -23,13 +23,14 @@ object Phonebook extends App {
 
   type Information = String
 
-  type Storage = Map[Name, Information]
+  type Storage = (Int, Map[Name, Information])
 
   type Failure = String
 
-  // // We want some state
+  // We want some state
   type PhonebookState[A] = StateT[Id, Storage, A]
 
+  // We want some error handling
   type PhonebookT[A] = EitherT[PhonebookState, Failure, A]
 
   sealed abstract class Command
@@ -37,34 +38,52 @@ object Phonebook extends App {
   case class Remove(name: String) extends Command
   case class Lookup(name: String) extends Command
 
-  def executeCommand(cmd: Command): PhonebookT[Information] = cmd match {
+  def executeCommand(cmd: Command): PhonebookT[Information] = liftStateTtoEitherT(cmd match {
 
-    case Lookup(name) => liftStateTtoEitherT( for {
-        s  <- init[Storage]
-      } yield s.get(name).map( Right(_) ).getOrElse(Left("Not Found in phonebook")))
+    case Lookup(name) => for {
+        s         <- tick()
+        (_, book) =  s
+        rslt      =  book.get(name).map( Right(_) ).getOrElse(Left("Not Found in phonebook"))
+      } yield rslt
 
-    case Remove(name) => liftStateTtoEitherT(for {
-        s  <- init[Storage]
-        _  <- put(s - name)
-      } yield (Right("Successfully removed " + name): Either[Failure,Information]))
+    case Remove(name) => for {
+        s          <- tick()
+        (cnt,book) =  s
+        _          <- put( (cnt, book - name) )
+        rslt       =  Right("Successfully removed " + name)
+      } yield rslt
 
-    case Add(name, information) => liftStateTtoEitherT(for {
-      s <- init[Storage]
-      _ <- put(s.updated(name, information))
-    } yield (Right("Successfully added a person " + name): Either[Failure,Information]))
+    case Add(name, information) => for {
+      s           <- tick()
+      (cnt, book) =  s
+      _           <- put( (cnt, book.updated(name, information)) )
+      rslt        =  Right("Successfully added a person " + name)
+    } yield rslt
 
-  }
+  })
 
   def bulkExecute(cmds: List[Command]): PhonebookT[Information] = cmds match {
-    case Nil     => "Successfully Executed All Commands".point[PhonebookT]
+    case Nil => liftStateTtoEitherT(for {
+      s        <- init[Storage]
+      (cnt, _) = s
+      rslt     = Right("Successfully Executed All %d Commands".format(cnt))
+    } yield rslt)
+
     case x :: xs => executeCommand(x) flatMap { _ => bulkExecute(xs) }
   }
 
-  def liftStateTtoEitherT[A](st: PhonebookState[Either[Failure, A]]): PhonebookT[A] = {
-    EitherT[PhonebookState, Failure, A](st)
-  }
+  def tick(): PhonebookState[Storage] = for {
+    s              <- init[Storage]
+    (cnt, storage) =  s
+    newS           <- put( (cnt+1, storage))
+  } yield newS
 
-  println(executeCommand(Lookup("mads")).run.eval(HashMap[Name, Information](("mads" -> "21"))))
+  def liftStateTtoEitherT[A](st: PhonebookState[Either[Failure, A]]): PhonebookT[A] = 
+    EitherT[PhonebookState, Failure, A](st)
+
+  println(executeCommand(Lookup("mads")).run.eval(
+    (0, HashMap[Name, Information](("mads" -> "21")))
+  ))
   
   println(
     bulkExecute(
@@ -72,7 +91,9 @@ object Phonebook extends App {
         Add("mads","21"),
         Lookup("mads")
       )
-    ).run.eval(HashMap[Name, Information]())
+    ).run.eval(
+      (0, HashMap[Name, Information]())
+    )
   )
 
 }
